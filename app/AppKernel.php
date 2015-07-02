27576878afc518b575660767753ec03663adacc5
Богомья Дмитрий <d.bogomya@rozumsoft.by>
2015-07-02 16:22:00 +0300

    j:TCCALLBACK-1 Изменил вендоры, доработал ansible

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new Callback\AppBundle\AppBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AppBundle;
+namespace Callback\AppBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
