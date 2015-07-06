6abee23f7e74cd39876c8c8767fd6c7f31eb62b2
Богомья Дмитрий <d.bogomya@rozumsoft.by>
2015-07-06 17:55:45 +0300

    j:TCCALLBACK-2 Изменил название таблицы user из-за конфликта с pgsql

--- app/migrations/Version20150706175336.php
+++ app/migrations/Version20150706175336.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20150706124102 extends AbstractMigration
+class Version20150706175336 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -18,15 +18,15 @@ class Version20150706124102 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE SEQUENCE user_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE users_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE user_session_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE "user" (id INT NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, salt VARCHAR(255) NOT NULL, roles TEXT NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON "user" (email)');
-        $this->addSql('COMMENT ON COLUMN "user".roles IS \'(DC2Type:array)\'');
+        $this->addSql('CREATE TABLE users (id INT NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, salt VARCHAR(255) NOT NULL, roles TEXT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_1483A5E9E7927C74 ON users (email)');
+        $this->addSql('COMMENT ON COLUMN users.roles IS \'(DC2Type:array)\'');
         $this->addSql('CREATE TABLE user_session (id INT NOT NULL, user_id INT DEFAULT NULL, token VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8849CBDE5F37A13B ON user_session (token)');
         $this->addSql('CREATE INDEX IDX_8849CBDEA76ED395 ON user_session (user_id)');
-        $this->addSql('ALTER TABLE user_session ADD CONSTRAINT FK_8849CBDEA76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE user_session ADD CONSTRAINT FK_8849CBDEA76ED395 FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     /**
@@ -39,9 +39,9 @@ class Version20150706124102 extends AbstractMigration
 
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE user_session DROP CONSTRAINT FK_8849CBDEA76ED395');
-        $this->addSql('DROP SEQUENCE user_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE users_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE user_session_id_seq CASCADE');
-        $this->addSql('DROP TABLE "user"');
+        $this->addSql('DROP TABLE users');
         $this->addSql('DROP TABLE user_session');
     }
 }
